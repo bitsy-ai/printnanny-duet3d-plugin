dd4d9fd442e81a95f9f69d552414824dfcc17145
Leigh Johnson <leigh@bitsy.ai>
2023-08-10 16:03:18 -0700

    run pre-commit hooks

--- dsf/http_endpoints.py
+++ dsf/http_endpoints.py
@@ -4,10 +4,10 @@ from dsf.connections import CommandConnection
 from dsf.http import HttpEndpointConnection, HttpEndpointType
 
 
-async def get_connection_status(http_endpoint_connection: HttpEndpointConnection):
-    await http_endpoint_connection.read_request()
-    await http_endpoint_connection.send_response(200, "ok")
-    http_endpoint_connection.close()
+async def get_connection_status(endpoint: HttpEndpointConnection):
+    await endpoint.read_request()
+    await endpoint.send_response(200, "ok")
+    endpoint.close()
 
 
 def register_http_endpoints():
@@ -15,11 +15,14 @@ def register_http_endpoints():
     cmd_conn.connect()
 
     # Register the connection status endpoint
-    endpoint = cmd_conn.add_http_endpoint(HttpEndpointType.GET, "printnanny", "connection-status")
+    endpoint = cmd_conn.add_http_endpoint(
+        HttpEndpointType.GET, "printnanny", "connection-status"
+    )
     # Register the handler to reply on requests
     endpoint.set_endpoint_handler(get_connection_status)
 
     hostname = socket.gethostname()
-    print(f"Connection status available from http://{hostname}/machine/printnanny/connection-status")
+    url = f"http://{hostname}/machine/printnanny/connection-status"
+    print(f"Connection status available from {url}")
 
     return cmd_conn, endpoint
