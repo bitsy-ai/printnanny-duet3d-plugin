69415b5f42edb04f977b3a243e52679bdb7bb520
Leigh Johnson <leigh@bitsy.ai>
2023-08-12 12:08:34 -0700

    fix buttons

--- src/PrintNannySettings.vue
+++ src/PrintNannySettings.vue
@@ -9,30 +9,15 @@
             <v-card-text>
               Copy/paste your printer's client id and client secret below.
 
-              <v-text-field
-                v-model="clientId"
-                required
-                label="Client ID"
-              ></v-text-field>
-              <v-text-field
-                v-model="clientSecret"
-                type="password"
-                required
-                label="Client Secret"
-              ></v-text-field>
-              <v-text-field
-                v-model="apiUrl"
-                required
-                label="URL"
-              ></v-text-field>
+              <v-text-field v-model="clientId" required label="Client ID"></v-text-field>
+              <v-text-field v-model="clientSecret" type="password" required label="Client Secret"></v-text-field>
+              <v-text-field v-model="apiUrl" required label="URL"></v-text-field>
             </v-card-text>
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" @click="testConnection"
-                >Test Connection</v-btn
-              >
-              <v-btn color="blue darken-1" type="submit">Save</v-btn>
+              <v-btn color="indigo darken-1" @click="testConnection">Test Connection</v-btn>
+              <v-btn color="grey darken-1" type="submit">Save</v-btn>
             </v-card-actions>
           </v-form>
         </v-card>
@@ -70,17 +55,17 @@ export default {
   mounted() {
     this.clientId =
       this.plugins.PrintNannyDuetPlugin &&
-      this.plugins.PrintNannyDuetPlugin.clientId
+        this.plugins.PrintNannyDuetPlugin.clientId
         ? this.plugins.PrintNannyDuetPlugin.clientId
         : "";
     this.clientSecret =
       this.plugins.PrintNannyDuetPlugin &&
-      this.plugins.PrintNannyDuetPlugin.clientSecret
+        this.plugins.PrintNannyDuetPlugin.clientSecret
         ? this.plugins.PrintNannyDuetPlugin.clientSecret
         : "";
     this.apiUrl =
       this.plugins.PrintNannyDuetPlugin &&
-      this.plugins.PrintNannyDuetPlugin.apiUrl
+        this.plugins.PrintNannyDuetPlugin.apiUrl
         ? this.plugins.PrintNannyDuetPlugin.apiUrl
         : "";
   },
