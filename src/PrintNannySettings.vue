f976ca35ab0beb196ba51c8c57b6b2006e9f5d04
Leigh Johnson <leigh@printnanny.ai>
2023-08-14 11:37:27 -0700

    try uploading .json

--- src/PrintNannySettings.vue
+++ src/PrintNannySettings.vue
@@ -2,25 +2,7 @@
   <v-container>
     <v-row>
       <v-col cols="12" md="6">
-        <v-card>
-          <v-form ref="form" @submit.prevent="submit">
-            <v-card-title class="headline"> PrintNanny Settings </v-card-title>
-
-            <v-card-text>
-              Copy/paste your printer's client id and client secret below.
-
-              <v-text-field v-model="clientId" required label="Client ID"></v-text-field>
-              <v-text-field v-model="clientSecret" type="password" required label="Client Secret"></v-text-field>
-              <v-text-field v-model="apiUrl" required label="URL"></v-text-field>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="indigo darken-1" @click="testConnection">Test Connection</v-btn>
-              <v-btn color="grey darken-1" type="submit">Save</v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card>
+        <PrintNannyCredentialsUpload />
       </v-col>
     </v-row>
   </v-container>
@@ -28,66 +10,12 @@
 
 <script>
 "use strict";
-import { mapState, mapMutations } from "vuex";
-// import * as api from "printnanny-factory-rest-api";
-// import { logGlobal } from "@/utils/logging";
-import { getJwt } from "./utils";
+import PrintNannyCredentialsUpload from "./PrintNannyCredentialsUpload.vue";
 
 export default {
-  data() {
-    return {
-      error: undefined,
-      clientId: "",
-      clientSecret: "",
-      apiUrl: "https://v2.printnanny.ai",
-    };
-  },
-  computed: {
-    ...mapState("settings", ["plugins"]),
-  },
-  watch: {
-    // connectDialogShown(to) { this.shown = to; },
-    // lastHostname(to) {
-    //     // Update the hostname
-    //     this.hostname = to;
-    // }
-  },
-  mounted() {
-    this.clientId =
-      this.plugins.PrintNannyDuetPlugin &&
-        this.plugins.PrintNannyDuetPlugin.clientId
-        ? this.plugins.PrintNannyDuetPlugin.clientId
-        : "";
-    this.clientSecret =
-      this.plugins.PrintNannyDuetPlugin &&
-        this.plugins.PrintNannyDuetPlugin.clientSecret
-        ? this.plugins.PrintNannyDuetPlugin.clientSecret
-        : "";
-    this.apiUrl =
-      this.plugins.PrintNannyDuetPlugin &&
-        this.plugins.PrintNannyDuetPlugin.apiUrl
-        ? this.plugins.PrintNannyDuetPlugin.apiUrl
-        : "";
-  },
-  methods: {
-    ...mapMutations("settings", ["update"]),
-    submit() {
-      if (this.$refs.form.validate()) {
-        this.update({
-          plugins: {
-            PrintNannyDuetPlugin: {
-              clientId: this.clientId,
-              clientSecret: this.clientSecret,
-              apiUrl: this.apiUrl,
-            },
-          },
-        });
-      }
-    },
-    testConnection() {
-      const jwt = getJwt(this.clientId, this.clientSecret, this.apiUrl);
-      console.log("Got JWT", jwt);
-    },
-  },
+  data() { },
+  computed: {},
+  mounted() { },
+  methods: {}
 };
 </script>
