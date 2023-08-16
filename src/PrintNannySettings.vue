e2954aca382e0deb0333fc934d6891be18ee9f83
Leigh Johnson <leigh@printnanny.ai>
2023-08-15 18:26:27 -0700

    fix if condition

--- src/PrintNannySettings.vue
+++ src/PrintNannySettings.vue
@@ -3,27 +3,30 @@
     <v-row>
       <v-col cols="12" md="6">
         <v-card>
-            <v-card-title class="headline"> Application Key </v-card-title>
+          <v-card-title class="headline"> Application Key </v-card-title>
 
-            <v-card-text v-if="!credentialFile">
-              Upload the JSON file you downloaded when creating a PrintNanny
-              application key.
+          <v-card-text v-if="!credentialFile">
+            Upload the JSON file you downloaded when creating a PrintNanny
+            application key.
 
-              <upload-btn ref="mainUpload" class="hidden-sm-and-down" :elevation="1" :directory="directory" :target="uploadTarget" color="primary"></upload-btn>
+            <upload-btn
+              ref="mainUpload"
+              class="hidden-sm-and-down"
+              :elevation="1"
+              :directory="directory"
+              :target="uploadTarget"
+              color="primary"
+            ></upload-btn>
+          </v-card-text>
 
-            </v-card-text>
+          <v-card-text v-else> Using credentials file: {{}} </v-card-text>
 
-            <v-card-text v-else>
-              Using credentials file: {{  }}
-            </v-card-text>
-
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" type="submit" @click="submit"
-                >Save & Test Connection</v-btn
-              >
-            </v-card-actions>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" type="submit" @click="submit"
+              >Save & Test Connection</v-btn
+            >
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -34,19 +37,19 @@
 "use strict";
 import { mapState, mapMutations, mapGetters } from "vuex";
 // import { logGlobal } from "@/utils/logging";
-import Path from '@/utils/path'
+import Path from "@/utils/path";
 
 export default {
   data() {
     return {
       credentialFile: null,
-      directory: `${Path.system}}/PrintNannyDuetPlugin/`
+      directory: `${Path.system}}/PrintNannyDuetPlugin/`,
     };
   },
   computed: {
-    ...mapState('machine/model', {
-			systemDirectory: state => state.directories.systemDirectory
-		}),
+    ...mapState("machine/model", {
+      systemDirectory: (state) => state.directories.systemDirectory,
+    }),
     ...mapState("settings", ["plugins"]),
     ...mapGetters("machine", ["connector"]),
   },
